945f26e3c8d10c54e93943ca41950e8861dd77ac
Ivan Fetch <ivan@fairwinds.com>
2023-01-23 13:24:43 -0700

    Move order of some resources around

--- tf2/s3.tf
+++ tf2/s3.tf
@@ -1,8 +1,10 @@
-resource "aws_s3_bucket" "data_exchange" {
-  bucket_prefix = var.name_prefix
-}
-
 resource "aws_s3_bucket_acl" "data_exchange" {
   bucket = aws_s3_bucket.data_exchange.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "data_exchange" {
+  bucket_prefix = var.name_prefix
+}
+
+
--- tf2/sg.tf
+++ tf2/sg.tf
@@ -2,6 +2,13 @@ resource "aws_security_group" "ssh" {
   name_prefix = "${var.name_prefix}-"
   description = "Allow inbound SSH"
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -9,12 +16,6 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = var.allowed_ssh_cidrs
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   vpc_id = module.vpc.vpc_id
 }
